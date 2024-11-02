2d71699833c5794a3b6fb63c7920e1b2bffdf3c0
margaritasentuna <margaritasentuna@gmail.com>
2024-11-02 08:42:22 +0000

    Week 2

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import os
  DO NOTE: some functionalities will still be vulnerable even if the value is set to 0
           as it is a matter of bad practice. Such an example is the debug endpoint.
 '''
+
 vuln = int(os.getenv('vulnerable', 1))
 # vuln=1
-# token alive for how many seconds?
+# token alive for how many seconds?$env:vulnerable=0; python3 app.py
 alive = int(os.getenv('tokentimetolive', 60))
 
 
